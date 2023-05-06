c1518ee0ec5c598d6ac01061402a407f29dfb497
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-05-06 22:50:29 +0530

    a="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z" n=int(input())
    for i in range(n-1,-1,-1):
        k=a[2*i]+" "
        print(k*(i+1))

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,9 @@
+a="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 n=int(input())
 
-for i in range(1,n+1):
-    s=""
-    for j in range(1,i+1):
-        s=s+str(j)
-    print(s)
+for i in range(n-1,-1,-1):
+    k=a[2*i]+" "
+    print(k*(i+1))
+    
         
         
