8d5a2a3b4c33d6f8a2ecbbc4c3b305c5a45b1c6c
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-05-06 22:58:03 +0530

    n=int(input())
    for i in range(1,n+1):
        print(" "*(n-i)+str(i)*((2*i)-1))

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,11 +1,4 @@
-a="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 n=int(input())
 
-for i in range(0,n):
-    s=""
-    for j in range(1,n+1):
-        s=s+str(a[2*i])+" "
-    print(s)
-    
-        
-        
+for i in range(1,n+1):
+    print(" "*(n-i)+str(i)*((2*i)-1))
