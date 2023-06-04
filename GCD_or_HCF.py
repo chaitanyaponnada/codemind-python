8a1511466346f2f1dab5f88ba831e3c860df76c3
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-06-04 23:32:54 +0530

    a,b=map(int,input().split()) if(a>b):     m=a else:     m=b gcd=m for i in range(1,a*b+1):     if(a%i==0 and b%i==0):         gcd=i print(gcd)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,12 +1,10 @@
 a,b=map(int,input().split())
-
-if a>b:
-    gcd=b
+if(a>b):
+    m=a
 else:
-    gcd=a
-    
-s=gcd
-for i in range(1,s+1):
+    m=b
+gcd=m
+for i in range(1,a*b+1):
     if(a%i==0 and b%i==0):
-        s=i
-print(s)
+        gcd=i
+print(gcd)        
