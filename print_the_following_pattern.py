1f0703dd6bd73a905a06ca68e7bb9920699068db
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-06-06 01:42:36 +0530

    n=int(input()) s="" for i in range(1,n+1):     if(i==1):         print("*")     elif(i==n):         print("*"*n)     else:         print("*"+" "*(i-2)+"*")

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,10 +1,9 @@
 n=int(input())
+s=""
 for i in range(1,n+1):
-    s=""
-    for j in range(1,n+1):
-        if i==j:
-            s=s+"0"
-        else:
-            s=s+"x"
-    print(s)
-            
+    if(i==1):
+        print("*")
+    elif(i==n):
+        print("*"*n)
+    else:
+        print("*"+" "*(i-2)+"*")
