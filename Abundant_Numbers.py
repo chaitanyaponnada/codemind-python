d13e57661cc1212d589222f4ea21bbca75194b10
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-06-19 21:28:36 +0530

    def is_abundant_number(n):     proper_divisors_sum = 0
        for i in range(1, n):
            if n % i == 0:
                proper_divisors_sum += i
        
        return proper_divisors_sum > n
    n=int(input())
    if is_abundant_number(n):
        print(True)
    else:
        print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,8 +1,13 @@
-n=int(input())
-l=[]
-for i in range(1,n):
-    if(n%i==0):
-        l.append(i)
-z=sum(l)
-print(z>n)
+def is_abundant_number(n):
+    proper_divisors_sum = 0
     
+    for i in range(1, n):
+        if n % i == 0:
+            proper_divisors_sum += i
+    
+    return proper_divisors_sum > n
+n=int(input())
+if is_abundant_number(n):
+    print(True)
+else:
+    print(False)
