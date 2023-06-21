7adc5b7901c05dece5b92c1e26b0c3d5fdd586ef
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-06-21 18:17:03 +0530

    m=int(input()) n=int(input()) l=[] d=[] for i in range(1,m):     if(m%i==0):         l.append(i) for i in range(1,n):     if(n%i==0):         d.append(i) if(sum(l)==n) and(sum(d)==m):     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -5,13 +5,11 @@ d=[]
 for i in range(1,m):
     if(m%i==0):
         l.append(i)
-for j in range(1,n):
-    if(n%j==0):
-        d.append(j)
-x=sum(l)
-y=sum(d)
-if(int(n)==int(x)) and (int(m)==int(y)):
+for i in range(1,n):
+    if(n%i==0):
+        d.append(i)
+if(sum(l)==n) and(sum(d)==m):
     print("Amicable")
 else:
     print("Not Amicable")
-# print(x,n,y,m)
+        
