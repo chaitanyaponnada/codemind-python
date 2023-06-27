1ba394066ca1e1117ed2602c0c86f78b6eb49fb8
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-06-27 19:03:53 +0530

    n=int(input()) a=list(map(int,input().split())) l=[] s=set(a) # print(s)
    d=[]
    for i in s:
        c=0
        i=int(i)
        for j in a:
            j=int(j)
            if(i==j):
                c=c+1
        if(c==1):
            d.append(i)
    z=len(d)
    if(z==0):
        print(-1)
    else:
        print(max(d))

---
+++ UNIQUE_MAXIMUM_NUMBER.py
@@ -0,0 +1,24 @@
+n=int(input())
+a=list(map(int,input().split()))
+l=[]
+s=set(a)
+# print(s)
+
+d=[]
+for i in s:
+    c=0
+    i=int(i)
+    for j in a:
+        j=int(j)
+        if(i==j):
+            c=c+1
+    if(c==1):
+        d.append(i)
+z=len(d)
+if(z==0):
+    print(-1)
+else:
+    print(max(d))
+
+        
+    
