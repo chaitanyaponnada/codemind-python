b6dcd6bedd98681a454bf5a01d617a3498f5307c
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-06-29 21:58:20 +0530

    def ugly(num):     if num <= 0:         return False
        while num % 2 == 0:
            num //= 2
        while num % 3 == 0:
            num //= 3
        while num % 5 == 0:
            num //= 5
    
        return num == 1
    
    n=int(input())
    if(ugly(n)):
        print("Ugly Number")
    else:
        print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,18 @@
+def ugly(num):
+    if num <= 0:
+        return False
+
+    while num % 2 == 0:
+        num //= 2
+    while num % 3 == 0:
+        num //= 3
+    while num % 5 == 0:
+        num //= 5
+
+    return num == 1
+
+n=int(input())
+if(ugly(n)):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
