dc9a0b43a965e363836ed131dcc5414eadfdba84
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-07-03 11:52:15 +0530

    def prime(n):     if(n<=1):         return False     for i in range(2,int(n**0.5)+1):         if(n%i==0):             return False             break     return True
    def nop(m,n):
        p=[]
        for i in range(m,n+1):
            if prime(i):
                p.append(i)
        return p
    
       
    a=int(input())
    b=int(input())
    
    print(len(nop(a,b)))

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,21 @@
-def prime(a):
-    if a==1:
+def prime(n):
+    if(n<=1):
         return False
-    for i in range(2,int(a**0.5)+1):
-        if a%i==0:
+    for i in range(2,int(n**0.5)+1):
+        if(n%i==0):
             return False
+            break
     return True
-a = int(input())
-b = int(input())
-p=0
-while a<=b:
-    if prime(a):
-        p+=1
-    a+=1
-print(p)
+    
+def nop(m,n):
+    p=[]
+    for i in range(m,n+1):
+        if prime(i):
+            p.append(i)
+    return p
+
+   
+a=int(input())
+b=int(input())
+
+print(len(nop(a,b)))
