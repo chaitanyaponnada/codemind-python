50c2c2c213a84caffdabcb24614c747867157fcc
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-07-05 00:29:54 +0530

    def prime(n):     if(n<=1):         return False     for i in range(2,n):         if(n%i==0):             return False             break     return True
    a=int(input())
    l=[]
    z=[]
    for i in range(1,a+1):
        if(a%i==0):
            l.append(i)
            
    for j in l:
        if(prime(j)==False):
            z.append(j)
    print(len(z))

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,20 +1,21 @@
 def prime(n):
-    if(n==1):
+    if(n<=1):
         return False
-    else:
-        for i in range(2,n):
-            if(n%i==0):
-                return False
-                break
-        return True
-
-n=int(input())
+    for i in range(2,n):
+        if(n%i==0):
+            return False
+            break
+    return True
+    
+a=int(input())
 l=[]
-res=[]
-for i in range(1,n+1):
-    if(n%i==0):
+z=[]
+for i in range(1,a+1):
+    if(a%i==0):
         l.append(i)
-for i in l:
-    if(prime(i)==False):
-        res.append(i)
-print(len(res))
+        
+for j in l:
+    if(prime(j)==False):
+        z.append(j)
+print(len(z))
+        
