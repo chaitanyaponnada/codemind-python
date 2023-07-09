ee74d78444e421495d66375b81546317dc707e56
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-07-09 09:52:15 +0530

    def prime(n):     n=int(n)     if(n<=1):         return False     for i in range(2,n):         if(n%i==0):             return False     return True
    n=int(input())
    z=str(n)
    z=z[::-1]
    if(prime(n) and prime(z)):
        print("circular prime")
    elif(prime(n) and prime(z)==False):
        print("prime but not a circular prime")
    else:
        print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,22 +1,18 @@
 def prime(n):
-    if(n==1):
+    n=int(n)
+    if(n<=1):
         return False
-    else:
-        for i in range(2,n):
-            if(n%i==0):
-                return False
-                break
+    for i in range(2,n):
+        if(n%i==0):
+            return False
     return True
-
+    
 n=int(input())
-s=prime(n)
-n=str(n)
-n=n[::-1]
-n=int(n)
-k=prime(n)
-if(s==True and k==True):
+z=str(n)
+z=z[::-1]
+if(prime(n) and prime(z)):
     print("circular prime")
-elif(s==True):
+elif(prime(n) and prime(z)==False):
     print("prime but not a circular prime")
 else:
     print("not prime")
